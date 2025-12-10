f76a82cf79dcfea58329747e14fdb17bef09b7b2
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2025-12-10 15:11:57 +0800

    build: install jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,11 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    languageOptions: { globals: globals.jest },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
   },
   globalIgnores(["webpack.*", "dist/"]),
 ]);
