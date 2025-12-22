dad167bbbd9509d07ffba551a036188eb99cea79
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2025-12-22 15:47:26 +0800

    feat: add attack container to renderGameboard function

--- src/modules/ui/renderGameboard.js
+++ src/modules/ui/renderGameboard.js
@@ -1,24 +1,50 @@
 import Ship from "../ship.js";
 
-export default function renderGameboard(grid) {
+export default function renderGameboard(player) {
+  const playerName = player.name;
+  const playerGrid = player.gameboard.grid;
   const container = document.querySelector(".container");
   const gameboardContainer = document.createElement("div");
-  gameboardContainer.classList.add("gameboard-container");
+  const shipTitle = document.createElement("h2");
+  const shipContainer = document.createElement("div");
+  const attackTitle = document.createElement("h2");
+  const attackContainer = document.createElement("div");
 
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      const el = grid[i][j];
+  gameboardContainer.classList.add("gameboard-container");
+  shipTitle.classList.add("ship-title");
+  shipContainer.classList.add("ship-container");
+  attackTitle.classList.add("attack-title");
+  attackContainer.classList.add("attack-container");
+
+  shipTitle.textContent = `${playerName}'s Ships`;
+  attackTitle.textContent = `${playerName}'s Attacks`;
+
+  for (let i = 0; i < playerGrid.length; i++) {
+    for (let j = 0; j < playerGrid[i].length; j++) {
+      const el = playerGrid[i][j];
       if (el instanceof Ship) {
         const ship = document.createElement("div");
         ship.classList.add("ship");
-        gameboardContainer.appendChild(ship);
+        shipContainer.appendChild(ship);
       } else {
         const sea = document.createElement("div");
         sea.classList.add("sea");
-        gameboardContainer.appendChild(sea);
+        shipContainer.appendChild(sea);
       }
     }
   }
 
+  for (let k = 0; k < 100; k++) {
+    const sea = document.createElement("div");
+    sea.classList.add("sea");
+    attackContainer.appendChild(sea);
+  }
+
+  gameboardContainer.append(
+    shipTitle,
+    shipContainer,
+    attackTitle,
+    attackContainer,
+  );
   container.appendChild(gameboardContainer);
 }
